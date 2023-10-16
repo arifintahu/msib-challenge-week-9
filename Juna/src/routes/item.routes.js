72e8c116d1bdde374a4faf85574675057c1c0070
Junatius <cjuniar0@gmail.com>
2023-10-17 00:18:32 +0800

    feat: create route PUT /items/:id

---
+++ Juna/src/routes/item.routes.js
@@ -0,0 +1,24 @@
+const { Router } = require("express");
+const { itemController } = require("../controllers");
+const { validator } = require("../middlewares");
+
+const router = Router();
+const { validate, requirements } = validator;
+
+router
+  .route("/")
+  .post([validate(requirements.createItem)], itemController.createItem);
+
+router
+  .route("/:id")
+  .get([validate(requirements.getItem)], itemController.getItem);
+
+// TODO: create route PUT /items/:id
+router
+  .route("/:id")
+  .put(
+    [validate(requirements.updateItem)],
+    itemController.updateItem
+  )
+
+module.exports = router;
