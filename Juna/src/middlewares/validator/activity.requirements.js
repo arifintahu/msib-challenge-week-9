724a3caceb5682c3605b1c3c9f6d0e063b8c6909
Junatius <cjuniar0@gmail.com>
2023-10-17 00:13:58 +0800

    feat: Add requirements for getActivityItems

---
+++ Juna/src/middlewares/validator/activity.requirements.js
@@ -0,0 +1,19 @@
+const { body, param, query } = require("express-validator");
+
+const requirements = {
+  createActivity: [body("title").isString().isLength({ min: 3 })],
+  getActivities: [
+    query("page").isInt({ min: 1 }).optional({ nullable: true }),
+    query("limit").isInt({ min: 10, max: 50 }).optional({ nullable: true }),
+  ],
+  getActivity: [param("id").isInt({ min: 1 })],
+
+  // TODO: Add requirements for getActivityItems
+  getActivityItems:[
+    param("id").isInt({ min: 1 }),
+    query("page").isInt({ min: 1 }).optional({ nullable: true }),
+    query("limit").isInt({ min: 1, max: 50 }).optional({ nullable: true }),
+  ]
+};
+
+module.exports = requirements;
